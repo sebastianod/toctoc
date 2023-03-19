4a480602977b04f26248f468a6fdebc7ce07f6ec
sebastian <sebastianochoad@gmail.com>
2023-03-19 16:36:25 -0500

    updated cloud functions to check for user being a teacher to set another teacher

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,15 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 // get user and add custom claim (teacher)
+// addTeacherRole will be accessible from client. onCall does that.
+// onCall( <callback> ), callback will fire when addTeacherRole is called
 exports.addTeacherRole = functions.https.onCall((data, context) => {
+  // context has info on the user that is triggering the function
+  // Check if user is a teacher, only they can set other teachers
+  if ( context.auth.token.teacher !== true ) {
+    return {error: "Only teachers can create teacher roles!"};
+  }
+
   return admin.auth().getUserByEmail(data.email).then((user) => {
     return admin.auth().setCustomUserClaims(user.uid, {
       teacher: true,
