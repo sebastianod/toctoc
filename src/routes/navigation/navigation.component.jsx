53bbd4f2b73038efe90a8f1d112a40940b7c35bc
sebastian <sebastianochoad@gmail.com>
2023-03-21 11:58:29 -0500

    prepared the new test-component and student components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -10,40 +10,44 @@ const Navigation = () => {
   const { currentUser } = useContext(UserContext); //to sign in or out
 
   return (
-    <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
-          <img className="logo" src={AudioWavesLogo} alt="TocToc" />
-        </Link>
-        <div className="nav-links-container">
-          <NavLink className="nav-link" to="/">
-            HOME
-          </NavLink>
-          {/* Show sign out if user is signed in, sign out if signed in */}
-          {currentUser ? (
-            <Fragment>
-              <NavLink className="nav-link" to="/dasht">
-                TEACHER DASHBOARD
-              </NavLink>
-              <NavLink className="nav-link" to="/dashs">
-                STUDENT DASHBOARD
-              </NavLink>
-              <span className="nav-link" onClick={signOutUser}>
-                SIGN OUT
-              </span>
-            </Fragment>
-          ) : (
-            <NavLink className="nav-link" to="/auth">
-              SIGN IN
+    <>
+  <div className="navbar-container">
+    <div className="navigation">
+      <Link className="logo-container" to="/">
+        <img className="logo" src={AudioWavesLogo} alt="TocToc" />
+      </Link>
+      <div className="nav-links-container">
+        <NavLink className="nav-link" to="/">
+          HOME
+        </NavLink>
+        {/* Show sign out if user is signed in, sign out if signed in */}
+        {currentUser ? (
+          <Fragment>
+            <NavLink className="nav-link" to="/dasht">
+              TEACHER DASHBOARD
             </NavLink>
-          )}
-        </div>
+            <NavLink className="nav-link" to="/dashs">
+              STUDENT DASHBOARD
+            </NavLink>
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          </Fragment>
+        ) : (
+          <NavLink className="nav-link" to="/auth">
+            SIGN IN
+          </NavLink>
+        )}
       </div>
-      <Outlet />
-      {/* <Outlet> component is used to render child routes within a parent route.
+    </div>
+    {/* <Outlet> component is used to render child routes within a parent route.
          It acts as a placeholder where the child routes will be rendered. */}
-    </Fragment>
+  </div>
+  <Outlet />
+</>
   );
 };
 
 export default Navigation;
+
+
