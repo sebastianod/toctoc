d415ac8601e09edbb3969294d324c65d1f4131e5
sebastian <sebastianochoad@gmail.com>
2023-03-23 01:56:52 -0500

    updated processListOfSentences in utilities.js

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -2,12 +2,12 @@ function capitalizeFirstLetterOfFirstWordAndLowercaseTheRest(string) { //takes a
     return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function processListOfSentences(string) {
+export function processListOfSentences(string) { //accepts a string of sentences delimited by "*" and returns an array of sentences with the first letter of each sentence capitalized and the rest lowercased
   return string
     .split("*") //split the string into an array of sentences delimited by "*"
     .map((sentence) => sentence.trim()) //remove any number of spaces from the beginning and end of each sentence
     .map((sentence) => {return capitalizeFirstLetterOfFirstWordAndLowercaseTheRest(sentence)}) //capitalize the first letter of each sentence and lower case the rest });
 }
 
-const testInput = "People* House*   stone* wAteR* RougH paTch"; //messed up test entry
-console.log(processListOfSentences(testInput));
+// const testInput = "People* House*   stone* wAteR* RougH paTch"; //messed up test entry
+// console.log(processListOfSentences(testInput));
