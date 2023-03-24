85d3b830a2f1a56a87d332195c78827b67141838
sebastian <sebastianochoad@gmail.com>
2023-03-24 11:55:43 -0500

    Added fixes for possible input errors like inputting any number of stars, or inputting empty sentences like  *   *

--- src/components/tests-component/test-details/test-details.component.jsx
+++ src/components/tests-component/test-details/test-details.component.jsx
@@ -1,29 +1,37 @@
 import { useContext, useState } from "react";
 import { TestContext } from "../../../contexts/test-context/test.context";
 import Button from "../../button/button.component";
+import {
+  processListOfSentences,
+  hasEmptynessBetweenStars,
+} from "../../../utils/utilities";
 import "./test-details.styles.scss";
 
 export default function TestDetails() {
   const { currentTest } = useContext(TestContext);
   const { name } = currentTest;
-
-  //setting the answers text area
   const [answersList, setAnswersList] = useState(
+    //setting the answers text area
     "Write* answers* here* separated* by* a* star*"
   );
-  //submmitting answers
-  const [submittedAnswers, setSubmittedAnswers] = useState([]);
+  const [submittedAnswers, setSubmittedAnswers] = useState([]); //submmitting answers
 
   const handleAnswerInput = (e) => {
     e.preventDefault();
-    const value = e.target.value
+    const value = e.target.value;
     setAnswersList(value);
   };
-console.log(answersList);
+  console.log(answersList);
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault(); //prevent page refresh, without this, the url changes to the action url and the page breaks
-    return setSubmittedAnswers(answersList);
+
+    if (hasEmptynessBetweenStars(answersList)) { //prevents submission of empty spaces between stars
+      return alert("Please remove empty spaces between stars");
+    }
+
+    const processedAnswers = processListOfSentences(answersList); //process the raw answersList to an array of sentences delimitted by a star
+    return setSubmittedAnswers(processedAnswers);
   }
   console.log(`Submitted ${name} test answers: `, submittedAnswers);
 
--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -4,6 +4,8 @@ function capitalizeFirstLetterOfFirstWordAndLowercaseTheRest(string) { //takes a
 
 export function processListOfSentences(string) { //accepts a string of sentences delimited by "*" and returns an array of sentences with the first letter of each sentence capitalized and the rest lowercased
   return string
+    .replace(/^\*+|\*+$/g, "") //error handling: remove all the beginning and ending stars if there are any
+    .replace(/\*+/g, "*") //error handling: replace all repeated stars with one star
     .split("*") //split the string into an array of sentences delimited by "*"
     .map((sentence) => sentence.trim()) //remove any number of spaces from the beginning and end of each sentence
     .map((sentence) => {return capitalizeFirstLetterOfFirstWordAndLowercaseTheRest(sentence)}) //capitalize the first letter of each sentence and lower case the rest });
@@ -11,3 +13,7 @@ export function processListOfSentences(string) { //accepts a string of sentences
 
 // const testInput = "People* House*   stone* wAteR* RougH paTch"; //messed up test entry
 // console.log(processListOfSentences(testInput));
+
+export function hasEmptynessBetweenStars(str) {
+  return /\*\s+\*/.test(str);
+}
