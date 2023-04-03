5b4b96589b535e644876245db3de6c2d7e0a34fd
sebastian <sebastianochoad@gmail.com>
2023-04-03 18:07:10 -0500

    Implemented excel user enrollment in each course through createStudentUnderCourse. Thus, there's a limit.

--- src/components/csv-input/excel-input.component.jsx
+++ src/components/csv-input/excel-input.component.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { read, utils } from "xlsx";
 import Button from "../button/button.component";
 import ExcelLogo from "../../assets/excel.png";
 import "./excel-input.styles.scss";
 import { addUsersFunction } from "../../utils/firebase/firebase-utils";
+import { createStudentUnderCourse } from "../../utils/firebase/firebase-utils";
 
 export default function ExcelInput(props) {
   const [jsonForm, setJsonForm] = useState(null);
+  const { type, courseId } = props;
 
-  const handleFileUpload = (e) => { //convert excel file to json
+  const handleFileUpload = (e) => {
+    //convert excel file to json
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.onload = (event) => {
@@ -27,28 +30,44 @@ export default function ExcelInput(props) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const { courseId, type } = props;
     if (type === "enrollment") {
       //enroll students under the course with courseId
-    } else if ( type === undefined ) { //no type specified
+      jsonForm.forEach(async (student) => {
+        try {
+          const email = student.email;
+          createStudentUnderCourse(courseId, email);
+        } catch (error) {
+          console.log(error.message);
+        }
+      });
+
+    } else if (type === undefined) {
       //create student user accounts
-    addUsersFunction(jsonForm)
-    .then((result) => {
-      console.log("Sercer Message: ", result);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+      addUsersFunction(jsonForm)
+        .then((result) => {
+          console.log("Server Message: ", result);
+          alert("Success creating users.");
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Error creating users.");
+        });
     }
   };
 
   return (
-    <div className="excel-input-container">
+    <div
+      className={
+        type === "enrollment"
+          ? "enrollment-excel-input-container"
+          : "excel-input-container"
+      }
+    >
       <h3 className="excel-input-label">Upload your excel file</h3>
-      <label htmlFor="file-input">
-        <img className="logo" src={ExcelLogo} alt="excel icon" />
+      <label htmlFor="file-input" className="excel-img-label">
+        <img className="excel-logo" src={ExcelLogo} alt="excel icon" />
       </label>
-      <form className="form-container" onSubmit={handleSubmit}>
+      <form className="excel-form-container" onSubmit={handleSubmit}>
         <input
           id="file-input"
           className="input"
