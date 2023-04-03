234a6cb72de9be24488eeba60482abb2dfd69f40
sebastian <sebastianochoad@gmail.com>
2023-04-02 19:20:47 -0500

    Changed the nav bar's UI based on if the user has the teacher property or not in <Navigation/>.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,45 +9,57 @@ import "./navigation.styles.scss";
 const Navigation = () => {
   const { currentUser } = useContext(UserContext); //to sign in or out
 
-  return (
-    <>
-  <div className="navbar-container">
-    <div className="navigation">
-      <Link className="logo-container" to="/">
-        <img className="logo" src={AudioWavesLogo} alt="TocToc" />
-      </Link>
-      <div className="nav-links-container">
-        <NavLink className="nav-link" to="/">
-          HOME
-        </NavLink>
-        {/* Show sign out if user is signed in, sign out if signed in */}
-        {currentUser ? (
+  function uiLogic() {
+    if (currentUser) { // if user is signed in
+      if (currentUser.teacher) { //if user is a teacher
+        return (
           <Fragment>
             <NavLink className="nav-link" to="/dasht">
               TEACHER DASHBOARD
-            </NavLink>
-            <NavLink className="nav-link" to="/dashs">
-              STUDENT DASHBOARD
             </NavLink>
             <span className="nav-link" onClick={signOutUser}>
               SIGN OUT
             </span>
           </Fragment>
-        ) : (
-          <NavLink className="nav-link" to="/auth">
-            SIGN IN
-          </NavLink>
-        )}
-      </div>
-    </div>
-    {/* <Outlet> component is used to render child routes within a parent route.
+        );
+      } else {
+        return (
+          <Fragment>
+            <NavLink className="nav-link" to="/dashs">
+              MY DASHBOARD
+            </NavLink>
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          </Fragment>
+        );
+      }
+    } else { //if user is not signed in
+      return (
+        <NavLink className="nav-link" to="/auth">
+          SIGN IN
+        </NavLink>
+      );
+    }
+  }
+
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <img className="logo" src={AudioWavesLogo} alt="TocToc" />
+          </Link>
+          <div className="nav-links-container">
+            {uiLogic()}
+          </div>
+        </div>
+        {/* <Outlet> component is used to render child routes within a parent route.
          It acts as a placeholder where the child routes will be rendered. */}
-  </div>
-  <Outlet />
-</>
+      </div>
+      <Outlet />
+    </>
   );
 };
 
 export default Navigation;
-
-
