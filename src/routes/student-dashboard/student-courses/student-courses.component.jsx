a73c38a0ddb081fe2f14caeec2f8ed7da35e2956
sebastian <sebastianochoad@gmail.com>
2023-04-12 15:36:31 -0500

    Created student-courses component. Here we'll fetch the enrollment document from each student. This document has the same id as the user's uid.

---
+++ src/routes/student-dashboard/student-courses/student-courses.component.jsx
@@ -0,0 +1,8 @@
+import "./student-courses.styles.scss";
+export default function StudentCourses() {
+  return (
+    <div>
+      <h2>List of student courses</h2>
+    </div>
+  );
+}
--- src/routes/student-dashboard/student-dashboard.component.jsx
+++ src/routes/student-dashboard/student-dashboard.component.jsx
@@ -1,11 +1,12 @@
-import Courses from "../../components/courses/courses.component";
+
+import StudentCourses from "./student-courses/student-courses.component";
 import "./student-dashboard.styles.scss";
 
 const StudentDashboard = () => {
   return (
     <div>
       <h1>Student Dashboard</h1>
-      <Courses />
+      <StudentCourses />
     </div>
   );
 };
