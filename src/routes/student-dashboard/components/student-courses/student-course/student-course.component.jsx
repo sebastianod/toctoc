ef02a367c1bfe6cb0a92f7d270b16c3963810fa2
sebastian <sebastianochoad@gmail.com>
2023-04-14 09:57:47 -0500

    Added a welcome area to student dashboard with the student's name and gave it some styling.

--- src/routes/student-dashboard/components/student-courses/student-course/student-course.component.jsx
+++ src/routes/student-dashboard/components/student-courses/student-course/student-course.component.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { capitalizeFirstLetterOfEachWord } from "../../../../../utils/utilities";
 import "./student-course.styles.scss";
 
 export default function StudentCourse(props) {
@@ -8,7 +9,7 @@ export default function StudentCourse(props) {
     <div className="student-course-container">
       <div className="student-course">
         <NavLink to={`/dashs/${courseId}`}>
-          <h3 className="student-course-title">{name}</h3>
+          <h3 className="student-course-title">{capitalizeFirstLetterOfEachWord(name)}</h3>
         </NavLink>
       </div>
     </div>
--- src/routes/student-dashboard/student-dashboard.component.jsx
+++ src/routes/student-dashboard/student-dashboard.component.jsx
@@ -1,11 +1,21 @@
 import StudentCourses from "./components/student-courses/student-courses.component";
+import { UserContext } from "../../contexts/user/user.context";
+import { useContext } from "react";
 import "./student-dashboard.styles.scss";
 
 const StudentDashboard = () => {
+  const { currentUser } = useContext(UserContext);
+  const studentName = currentUser.displayName;
+
   return (
-    <div>
-      <h1>Student Dashboard</h1>
-      <StudentCourses />
+    <div className="student-dashboard-container">
+      <div className="welcome-area">
+        <h1>Student Dashboard</h1>
+        <h3>Welcome {studentName}</h3>
+      </div>
+      <div className="student-courses-area">
+        <StudentCourses />
+      </div>
     </div>
   );
 };
