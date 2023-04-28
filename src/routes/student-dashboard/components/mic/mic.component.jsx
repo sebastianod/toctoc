1a4583cfc05d7323a9790fc72dba0ec55a9f5f33
sebastian <sebastianochoad@gmail.com>
2023-04-28 16:09:33 -0500

    Installed a few libraries for the audio processing: openai, @ffmpeg-installer/ffmpeg, fluent-ffmpeg, and mic.

--- src/routes/student-dashboard/components/mic/mic.component.jsx
+++ src/routes/student-dashboard/components/mic/mic.component.jsx
@@ -2,9 +2,14 @@ import "./mic.styles.scss";
 import MicSvg from "../../../../assets/mic.svg";
 
 export default function Mic() {
+
+  const handleClick = () => {
+    console.log("Mic clicked");
+  };
+
   return (
     <div className="mic-container">
-      <button className="mic">
+      <button className="mic" onClick={handleClick}>
         <img className="mic-svg" src={MicSvg} alt="mic" />
       </button>
     </div>
