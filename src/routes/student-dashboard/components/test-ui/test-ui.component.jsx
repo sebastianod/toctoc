6106c4ef3d64eaead55fa9cc9ede56603d12570a
sebastian <sebastianochoad@gmail.com>
2023-04-28 14:59:35 -0500

    Added cycling through the questions list in <TestUi/>

--- src/routes/student-dashboard/components/test-ui/test-ui.component.jsx
+++ src/routes/student-dashboard/components/test-ui/test-ui.component.jsx
@@ -7,37 +7,52 @@ import { useContext, useEffect, useState } from "react";
 import { capitalizeFirstLetterOfEachWord } from "../../../../utils/utilities";
 
 export default function TestUi() {
+  //fetch courseId and testId from context
   const { currentCourse } = useContext(CourseContext);
   const courseId = currentCourse.courseId;
   const { currentTest } = useContext(TestContext);
   const { testId, name } = currentTest;
-  const [currentQuestions, setCurrentQuestions] = useState([]);
+
+  // Question list and state for cycling through questions
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   // fetch Questions set by teacher
   useEffect(() => {
     const fetchQuestions = async () => {
       const questions = await getQuestions(courseId, testId);
       const questionsList = questions[0].questionsList;
-      setCurrentQuestions(questionsList);
+      setQuestions(questionsList);
     };
     fetchQuestions().catch((err) => console.log(err));
   }, [courseId, testId]);
 
+  // handle next question
+  const handleNextButton = () => {
+
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+    else if (currentQuestion === questions.length - 1) {
+      
+    }
+  }
+
   return (
     <div className="test-ui-container">
       <header className="header">
-        <span>1 of x</span>
+        <span>{currentQuestion + 1} of {questions.length}</span>
         <h3 className="test-title">{capitalizeFirstLetterOfEachWord(name)} Test</h3>
         <span>Quit</span>
       </header>
       <div className="question-area">
-        <strong>{currentQuestions[0]}</strong>
+        <strong>{questions[currentQuestion]}</strong>
       </div>
       <div className="response-area">
         <Mic />
       </div>
       <div className="footer">
-        <button className="next-skip">Next</button>
+        <button className="next-skip" onClick={handleNextButton} >Next</button>
       </div>
     </div>
   );
