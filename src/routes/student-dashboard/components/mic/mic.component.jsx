92941383982ecc6f79c30059c89bbfeac5c464e9
sebastian <sebastianochoad@gmail.com>
2023-04-29 19:49:30 -0500

    Created basic ui logic for the mic component. The states are: Available mic, unavailable mic, and recording mic. The maximum amount of repeats is two. No API callings yet.

--- src/routes/student-dashboard/components/mic/mic.component.jsx
+++ src/routes/student-dashboard/components/mic/mic.component.jsx
@@ -1,17 +1,81 @@
 import "./mic.styles.scss";
 import MicSvg from "../../../../assets/mic.svg";
+import MicUnavailableSvg from "../../../../assets/mic-unavailable.svg";
+import { useState } from "react";
 
 export default function Mic() {
+  const [isRecording, setIsRecording] = useState(false); //true when recording
+  const [hasRecorded, setHasRecorded] = useState(false); //true when recording has been done
+  //const [isNewQuestion, setIsNewQuestion] = useState(false); //To go in <TestUi />. Resets tries to 0 when next is pressed.
 
-  const handleClick = () => {
-    console.log("Mic clicked");
+  const maxTries = 2;
+  const [tries, setTries] = useState(0); //2 max tries
+
+  const handleMicClick = () => {
+    if (tries < maxTries) {
+      if (isRecording === true ) {
+        setIsRecording(!isRecording);
+        setHasRecorded(true);
+        setTries(tries + 1);
+      }
+      if (isRecording === false) {
+        setIsRecording(!isRecording);
+      }
+    }
+    if (tries === maxTries) {
+      setIsRecording(false);
+    }
   };
 
-  return (
-    <div className="mic-container">
-      <button className="mic" onClick={handleClick}>
-        <img className="mic-svg" src={MicSvg} alt="mic" />
-      </button>
-    </div>
-  );
+  console.log("tries: ", tries, "isRecording: ", isRecording);
+
+  const uiLogic = () => {
+    if (tries < maxTries && isRecording === false && hasRecorded === false) {
+      //Not recording and hasn't saved, default state
+      return (
+        <div className="mic-container">
+          <button className="mic available" onClick={handleMicClick}>
+            <img className="mic-svg" src={MicSvg} alt="mic" />
+          </button>
+        </div>
+      );
+    }
+
+    if (tries < maxTries && isRecording === true) {
+      //Recording
+      return (
+        <div className="mic-container">
+          <button className="mic recording" onClick={handleMicClick}>
+            <div className="recording-box"></div>
+          </button>
+        </div>
+      );
+    }
+
+    if (tries < maxTries && isRecording === false && hasRecorded === true) {
+      //Show play button and allow recording again
+      return (
+        <div className="mic-container">
+          <div className="play-container">▶️</div>
+          <button className="mic available" onClick={handleMicClick}>
+            <img className="mic-svg" src={MicSvg} alt="mic" />
+          </button>
+        </div>
+      );
+    }
+
+    if (tries === maxTries && isRecording === false) {
+      // disable mic but allow play of last recording
+      return (
+        <div className="mic-container">
+          <div className="play-container">▶️</div>
+          <button className="mic unavailable" onClick={handleMicClick}>
+            <img className="mic-svg" src={MicUnavailableSvg} alt="mic" />
+          </button>
+        </div>
+      );
+    }
+  };
+
+  return uiLogic();
 }
