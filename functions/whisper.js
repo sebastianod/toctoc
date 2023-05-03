9782be0de09ce3b68a935ea511b0fc9b411bbaa3
sebastian <sebastianochoad@gmail.com>
2023-05-02 22:45:14 -0500

    Changes to whisper.js and installed openAi and dotenv in the functions folder

--- functions/whisper.js
+++ functions/whisper.js
@@ -19,29 +19,30 @@ exports.whisper = functions.https.onRequest(
 
     // Set the parameters for the request
     const params = {
-      fileType: "wav", // specify the file type
       diarization: false, // optional, set to true if you want speaker diarization
       numSpeakers: 1, // optional, set the number of speakers if using diarization
-      url: audioUrl, // optional, set the URL of the audio file if using a URL instead of a file
       initialPrompt: "", // optional, set an initial prompt to teach the model phrases
       language: "en", // optional, set the language of the audio file
       task: "transcribe", // optional, set the task to transcribe or translate
     };
 
-    // Send the request to the OpenAI API
-    openai.whisper1
-      .transcriptions(params, audioFile)
-      .then((response) => {
-        // Handle the response
-        console.log(response.data);
-        // Send back the transcription as JSON
-        res.status(200).json(response.data);
-      })
-      .catch((error) => {
-        // Handle the error
-        console.error(error);
-        // Send back an error message as JSON
-        res.status(500).json({ error: error.message });
-      });
+    // If using a URL, add it to the params
+    if (audioUrl) {
+      params.url = audioUrl;
+    }
+
+    try {
+      // Send the request to the OpenAI API and await the response
+      const response = await openai.whisper1.transcriptions(params, audioFile);
+      // Handle the response
+      console.log(response.data);
+      // Send back the transcription as JSON
+      res.status(200).json(response.data);
+    } catch (error) {
+      // Handle the error
+      console.error(error);
+      // Send back an error message as JSON
+      res.status(500).json({ error: error.message });
+    }
   }
 );
