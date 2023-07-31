c6f7d9fb8c675563bc57359fbedb3ff421172a02
cuantumk <cuantumk@cuantumks-MacBook-Air.local>
2023-07-31 10:32:53 -0500

    Basic styling done to gradeDetails.

--- src/components/grades/grade-details/grade-details.component.jsx
+++ src/components/grades/grade-details/grade-details.component.jsx
@@ -1,15 +1,18 @@
 import styles from "./grade-details.module.scss";
+import ExcelIcon from "../../../assets/excel.svg";
 
 const GradeDetails = (props) => {
   const { testId, name, isAvailable } = props;
-    console.log(testId);
+  console.log(testId);
   return (
     <div className={styles.gradeContainer}>
       <div className={styles.testNameContainer}>
-        <span className={styles.testName}>{name}</span>
+        <h3 className={styles.testName}>{name}</h3>
       </div>
-      <div className={styles.availableIcon}>{isAvailable ? "🟢" : "🔴"}</div>
-      <div className={styles.excelIcon}></div>
+      <div className={styles.availabilityIcon}>{isAvailable ? "🟢" : "🔴"}</div>
+      <div className={styles.iconContainer}>
+        <img className={styles.excelIcon} alt="download excel file" src={ExcelIcon} />
+      </div>
     </div>
   );
 };
