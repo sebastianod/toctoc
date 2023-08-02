4dcee1f44680fa8ca4d0fa8b8f12a43263bdc2b2
cuantumk <cuantumk@cuantumks-MacBook-Air.local>
2023-08-02 16:19:48 -0500

    Modified the excel file to show grades to one decimal place. Also wait until the grades array is retrieved and use that for the excel file.

--- src/components/grades/grade-details/grade-details.component.jsx
+++ src/components/grades/grade-details/grade-details.component.jsx
@@ -1,65 +1,68 @@
 import styles from "./grade-details.module.scss";
 import ExcelIcon from "../../../assets/excel.svg";
 import { getAllGradesFromTest } from "../../../utils/firebase/firebase-utils";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { CourseContext } from "../../../contexts/course/course.context";
-import {utils, write} from "xlsx";
+import { utils, write } from "xlsx";
 
 const GradeDetails = (props) => {
   const { testId, name, isAvailable } = props;
   const { currentCourse } = useContext(CourseContext);
   const { courseId } = currentCourse;
-  const [ gradesList , setGradesList ] = useState([]);
 
   const handleExcelClick = async () => {
     const gradesArray = await getAllGradesFromTest(courseId, testId);
-    setGradesList(gradesArray);
-    console.log(gradesList);
-  
     // Call the function to create the Excel file and trigger the download
-    const testName = `${name} Test`
-    createExcelFile(testName);
+    const testName = `${name} Test`;
+    createExcelFile(gradesArray, testName);
   };
-  
-  const createExcelFile = (testName) => {
+
+  const createExcelFile = (gradesList, testName) => {
     // Create a new workbook and worksheet
     const workbook = utils.book_new();
-    const worksheet = utils.aoa_to_sheet([[testName],["Student's Name", "Grade %"]]);
-  
+    const worksheet = utils.aoa_to_sheet([
+      [testName],
+      ["Student Name", "Grade %"],
+    ]);
+
+    // Set the width of the first column (column A) to 20
+    worksheet["!cols"] = [{ width: 20 }];
+
     // Fill the worksheet with data from the gradesList array
     gradesList.forEach((gradeData, index) => {
-      const rowIndex = index + 2; // Start from the second row since the first row is for headers
+      const rowIndex = index + 2; // Start from the third row since the first two rows are for headers and test name
       const studentName = gradeData.student;
-      const grade = gradeData.grade;
-  
+      const grade = gradeData.grade.toFixed(1);
+
       // Set student name and grade in each row
-      utils.sheet_add_aoa(worksheet, [[studentName, grade]], { origin: `A${rowIndex + 1}` });
+      utils.sheet_add_aoa(worksheet, [[studentName, grade]], {
+        origin: `A${rowIndex + 1}`,
+      });
     });
-  
+
     // Add the worksheet to the workbook
     utils.book_append_sheet(workbook, worksheet, "Grades");
-  
+
     // Generate a buffer from the workbook
     const excelBuffer = write(workbook, { type: "buffer", bookType: "xlsx" });
-  
+
     // Create a Blob from the buffer
     const blob = new Blob([excelBuffer], {
       type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     });
-  
+
     // Create a URL for the Blob
     const url = URL.createObjectURL(blob);
-  
+
     // Create an anchor element to trigger the download
     const anchor = document.createElement("a");
     anchor.href = url;
     anchor.download = `${name}_testGrades.xlsx`;
     anchor.click();
-  
+
     // Release the URL resource
     URL.revokeObjectURL(url);
   };
-
 
   return (
     <div className={styles.gradeContainer}>
