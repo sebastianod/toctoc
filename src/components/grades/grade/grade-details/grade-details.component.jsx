3888e53a522f4637de5b76d625460a3fac967ab9
sebastianod <sebastianochoad@gmail.com>
2023-08-09 17:10:45 -0500

    Retrieving student grades within the gradeDetails component

--- src/components/grades/grade/grade-details/grade-details.component.jsx
+++ src/components/grades/grade/grade-details/grade-details.component.jsx
@@ -5,7 +5,7 @@ import { TestContext } from "../../../../contexts/test-context/test.context";
 import { getAllGradesFromTest } from "../../../../utils/firebase/firebase-utils";
 
 const GradeDetails = () => {
-  const [students, setStudents] = useState([]);
+  const [students, setStudents] = useState(null);
   const { currentCourse } = useContext(CourseContext);
   const courseId = currentCourse?.courseId;
   const { currentTest } = useContext(TestContext);
@@ -22,27 +22,22 @@ const GradeDetails = () => {
     getStudents();
   }, [courseId, testId]);
 
+  const showStudents = students.map((student, index) => {
+    const name = student.student;
+    const grade = student.grade;
+    return (
+      <div className={styles.studentContainer} key={index}>
+        <span className={styles.studentName}> {name} </span>
+        <span className={styles.grade}> {grade} %</span>
+      </div>
+    );
+  });
+
   return (
     <div className={styles.mainContainer}>
       <h3 className={styles.testName}>Test Name</h3>
       <br />
-      <div className={styles.studentsContainer}>
-        <div className={styles.studentContainer}>
-          <span className={styles.studentName}>Student name,</span>
-          <span className={styles.grade}> Grade %</span>
-        </div>
-        <br />
-        <div className={styles.studentContainer}>
-          <span className={styles.studentName}>Student name,</span>
-          <span className={styles.grade}> Grade %</span>
-        </div>
-        <br />
-        <div className={styles.studentContainer}>
-          <span className={styles.studentName}>Student name,</span>
-          <span className={styles.grade}> Grade %</span>
-        </div>
-        <br />
-      </div>
+      <div className={styles.studentsContainer}>{showStudents}</div>
     </div>
   );
 };
